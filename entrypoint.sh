a86ad6e385f8c1b4514c5de1559cdef8231c47f7
Donald Tyler <domhnall.tilliere@gmail.com>
2017-04-10 17:58:19 -0500

    Improve entrypoint.sh to adopt existing user
    Problem:
    If an LOCAL_USER_ID is provided that is already in use, entrypoint.sh will
    go ahead and create a new user with the same id. This can cause a bunch
    of issues (see last commit).
    
    Fix:
    I modified the entrypoint.sh file so that if the LOCAL_USER_ID exists in
    the system, then the script simply uses the existing account instead of
    creating another user.
    
    This is useful if you want to run the image as root. This might have been
    a potential alternative solution to the fix that the previous commit
    addressed, if it were not for the fact that user id# is so prevelant.
    
    We ideally want the image to be as portable as possible, and having user
    1000 already exist with a home of /home/node, but most other user ids
    ending up with a home of /home/user, means that it would not be possible
    to have a single script to run this container and mount ~/.ssh into
    a common location regardless of the Docker host.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
-# Credit: Deni Bertović https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
+# Based on: Deni Bertović https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
 
-# Add local user
-# Either use the LOCAL_USER_ID if passed in at runtime or
-# fallback
-
+# Use the LOCAL_USER_ID that was passed in, or fallback to 9001
 USER_ID=${LOCAL_USER_ID:-9001}
 
+# Get the username that is associated with the user id (if any)
+USER_NAME=`getent passwd | awk -F: '$3 == '${USER_ID}' { print $1 }'`
 
-if [ ! -d "/home/user" ]; then
-  ARGS='-m'
+# Does the user already exist?
+if [ "$USER_NAME" == "" ]; then
+  # No, we need to create it.
+  USER_NAME=user
+
+  # Create the home folder if it doesn't already exist.
+  if [ ! -d "/home/user" ]; then
+    ARGS='-m'
+  fi
+
+  # Create the user.
+  useradd --shell /bin/bash -u ${USER_ID} ${ARGS} user
+  export HOME=/home/${USER_NAME}
+
+  # Ensure home is owned by user (Docker may have created it as root when mounting volumes)
+  chown ${USER_NAME} $HOME
 fi
 
-useradd --shell /bin/bash -u $USER_ID -o -c "" ${ARGS} user
-export HOME=/home/user
-
-# Ensure home is owned by user (Docker may have created it as root when mounting volumes)
-chown user /home/user
-
 # Ensure workdir is owned and writeable by user
-chown user /workdir
+chown ${USER_NAME} /workdir
 chmod 700 /workdir
 
-gosu user "$@"
+# Execute the command
+gosu ${USER_NAME} "$@"
