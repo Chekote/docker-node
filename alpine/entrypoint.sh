8142e94028aed4490b5c56615ea40e205ea11082
Juan Mrad <juan@healthlabs.com>
2019-07-29 14:52:33 -0500

    move slim dockerfile to slim directory. create alpine image.

---
+++ alpine/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# Based on: Deni Bertović https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
+
+# Use the LOCAL_USER_ID that was passed in, or fallback to 9001
+USER_ID=${LOCAL_USER_ID:-9001}
+
+# Get the username that is associated with the user id (if any)
+USER_NAME=`getent passwd | awk -F: '$3 == '${USER_ID}' { print $1 }'`
+
+# Does the user already exist?
+if [ "$USER_NAME" == "" ]; then
+  # No, we need to create it.
+  USER_NAME=user
+
+  # Create the home folder if it doesn't already exist.
+  if [ ! -d "/home/user" ]; then
+    ARGS='-m'
+  fi
+
+  # Create the user.
+  useradd --shell /bin/bash -u ${USER_ID} ${ARGS} user
+  export HOME=/home/${USER_NAME}
+
+  # Ensure home is owned by user (Docker may have created it as root when mounting volumes)
+  chown ${USER_NAME} $HOME
+fi
+
+# Execute the command
+gosu ${USER_NAME} "$@"
